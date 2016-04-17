b9b5326f0a04de4532da99a5faf0b61cc44a26d6
hardyce <hardyce@tcd.ie>
2016-04-18 00:01:25 +0100

    created helper functions

--- imagerecognition.py
+++ imagerecognition.py
@@ -21,12 +21,23 @@ from pybrain.utilities           import percentError
 from pybrain.tools.shortcuts     import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.structure.modules   import SoftmaxLayer
-
+from pybrain.tools.customxml import NetworkWriter
+from pybrain.tools.customxml import NetworkReader
 from pylab import ion, ioff, figure, draw, contourf, clf, show, hold, plot
 from scipy import diag, arange, meshgrid, where
 from numpy.random import multivariate_normal
+# Using pickle
 
 
+def savenet(net):
+    fileObject = open('C:\\Users\\hardy_000\\Documents\\datasci\\net', 'w')
+
+    pickle.dump(net, fileObject)
+    NetworkWriter.writeToFile(net, 'C:\\Users\\hardy_000\\Documents\\datasci\\net.xml')
+    fileObject.close()
+def loadnet(net):
+    net = NetworkReader.readFrom('C:\\Users\\hardy_000\\Documents\\datasci\\net.xml')
+    return net
 #my_data = genfromtxt("C:\\Users\\hardy_000\\Downloads\\train.csv", delimiter=',')
 #x=pd.read_csv("C:\\Users\\hardy_000\\Downloads\\train.csv")
 def showpixel(i):
@@ -34,7 +45,8 @@ def showpixel(i):
     b=np.reshape(x.iloc[i,1:],(-1,28))
     imshow(b,cmap='Greys')
     print "label " + str(x.loc[i,'label'])
-   
+def getPercentError(data):
+    percentError(trainer.testOnClassData(dataset=data),data['class'])
 def loaddata():
     z= pd.read_csv("C:\\Users\\hardy_000\\Downloads\\train.csv")
     pixels=z.drop('label',1)
@@ -47,21 +59,12 @@ def loaddata():
 y,x=loaddata()
 z=ds.classification.ClassificationDataSet(784,nb_classes=10)
 
-    #print array[:,:-1]
-    #print array[:,-1]
-    #datase11t.addSample(array[:,:-1], array[:,-1])
-    #dataset.addSample(array[:,:-1], array[:,-2:-1])
 z.setField('input', x)
 z.setField('target', y)
+#following linke seems to be necessary to fill class field of dataset
+z._convertToOneOfMany( )
 fnn = buildNetwork( z.indim, 300, z.outdim, outclass=SoftmaxLayer )
-trainer = BackpropTrainer( fnn, dataset=z, momentum=0.1, verbose=True, weightdecay=0.01)
+trainer = BackpropTrainer( fnn, dataset=z, momentum=0.1, 
+                          verbose=True, weightdecay=0.01,learningrate=0.01,lrdecay=1)
 
 
-trainer.trainEpochs( 1 )
-
-for i in range(20):
-        trainer.trainEpochs(1)
-        trnresult = percentError(trainer.testOnClassData(), z['class'])
-        
-        print("epoch: %4d" % trainer.totalepochs,
-                     "  train error: %5.2f%%" % trnresult)
