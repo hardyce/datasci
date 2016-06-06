5742151d18c7358f0866958419b95723424714c8
hardyce <hardyce@tcd.ie>
2016-06-06 19:35:17 +0100

    added alpha based accuracy 3d plots

--- facebook.py
+++ facebook.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 import matplotlib as mpl
 from mpl_toolkits.mplot3d import Axes3D
-import discrete_cmap as dc
+
 def loadtraindata():
     z= pd.read_csv("C:\\Users\\hardy_000\\fbcomp\\train.csv")
     return z
@@ -18,13 +18,26 @@ def loadtestdata():
     z= pd.read_csv("C:\\Users\\hardy_000\\fbcomp\\test.csv")
 
     return z
+def discrete_cmap(N, base_cmap=None):
+    """Create an N-bin discrete colormap from the specified input map"""
+    # By Jake VanderPlas
+    # License: BSD-style
+    
+    # Note that if base_cmap is a string or None, you can simply do
+    #    return plt.cm.get_cmap(base_cmap, N)
+    # The following works for string, None, or a colormap instance:
 
-train=loadtraindata()
-places=train.groupby('place_id')
-meanplaces=places.mean()
-meanplaces=meanplaces.reset_index(drop=False)
+    base = plt.cm.get_cmap(base_cmap)
+    color_list = base(np.linspace(0, 1, N))
+    cmap_name = base.name + str(N)
+    return base.from_list(cmap_name, color_list, N)
+    
+
+#places=train.groupby('place_id')
+#meanplaces=places.mean()
+#meanplaces=meanplaces.reset_index(drop=False)
 #plot a few locations means
-meanplaces[:100].plot(kind='scatter',x='x',y='y',c='place_id',colormap='RdYlGn')
+#meanplaces[:100].plot(kind='scatter',x='x',y='y',c='place_id',colormap='RdYlGn')
 
 def getSamplePlaces(numberOfPlaces=1):
         
@@ -35,7 +48,10 @@ def getSamplePlace(numberOfPlaces=1):
 
 def normalize(series):
     return (series-min(series))/(max(series)-min(series))    
+
 numberOfPlaces=8
+
+train=loadtraindata()
 
 plt.figure(10, figsize=(14,16))
 cmapm = plt.cm.viridis
@@ -62,15 +78,24 @@ for i in range(numberOfPlaces):
     plt.title("pid: " + str(sample_place['place_id'])[0])
 plt.show()
 
-#fig = plt.figure()
-#ax = fig.add_subplot(111, projection='3d')
 
-sample=getSamplePlaces(10)
+sample=getSamplePlaces(30)
 norm = mpl.colors.Normalize(vmin=min(sample.place_id),vmax=max(sample.place_id))
-#ax.scatter(sample.x,sample.y,sample.time,alpha=sample.accuracy,marker='o')
 cp=plt.cm.get_cmap('jet')
-cp=dc.discrete_cmap(10,cp)
+cp=discrete_cmap(30,cp)
 cols=cp(norm(sample['place_id']))
 cols[:,3]=normalize(sample['accuracy'])
-plt.scatter(sample['x'],sample['y'],color=cols,)
+fig=plt.figure(11, figsize=(14,16))
+fig.add_subplot(111)
+plt.scatter(sample['x'],sample['y'],color=cols)
+plt.xlabel("X")
+plt.ylabel("Y")
 plt.show()
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+plt.xlabel('x')
+plt.ylabel('y')
+ax.set_zlabel('time')
+ax.scatter(sample.x,sample.y,sample.time,c=cols,marker='o',depthshade=False,lw = 0)
+
