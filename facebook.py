a04693187d5509d0720e994a38101a0c6ec996f4
hardyce <hardyce@tcd.ie>
2016-06-09 00:59:35 +0100

    included some accuracy weighted knn, have yet to implement ranking.

--- facebook.py
+++ facebook.py
@@ -11,7 +11,9 @@ import matplotlib as mpl
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn import neighbors, datasets
 from sklearn.neighbors import NearestNeighbors
-
+from sklearn import cross_validation
+from sklearn.metrics import pairwise
+from scipy.spatial import distance
 
 def loadtraindata():
     z= pd.read_csv("C:\\Users\\hardy_000\\fbcomp\\train.csv")
@@ -57,9 +59,9 @@ def plot3DScatter(data):
 #plot a few locations means
 #meanplaces[:100].plot(kind='scatter',x='x',y='y',c='place_id',colormap='RdYlGn')
 
-def getSamplePlaces(numberOfPlaces=1):
+def getSamplePlaces(data,numberOfPlaces=1):
         
-    return train[train.place_id.isin((train.place_id).unique()[1:numberOfPlaces+1])]
+    return data[data.place_id.isin((data.place_id).unique()[1:numberOfPlaces+1])]
 def getSamplePlace(numberOfPlaces=1):
         
     return train[train.place_id==(train.place_id).unique()[numberOfPlaces]]
@@ -97,7 +99,7 @@ for i in range(numberOfPlaces):
 plt.show()
 
 
-sample=getSamplePlaces(30)
+sample=getSamplePlaces(train,30)
 norm = mpl.colors.Normalize(vmin=min(sample.place_id),vmax=max(sample.place_id))
 cp=plt.cm.get_cmap('jet')
 cp=discrete_cmap(30,cp)
@@ -120,27 +122,44 @@ plt.show()
 
 #kmeans starts here
 train['time']=normalize(train['time'])
+train['accuracy']=normalize(train['accuracy'])
+train=getSquare(train,1,1)
+X_train, X_test = cross_validation.train_test_split(train, test_size=0.33, random_state=42)
 #train=train.drop('time',1)
-traintrain=train[0:14559010]
-small=getSquare(traintrain,1,1)
 #nd=normalize(small['accuracy'])
 #small=small[nd<0.1]
-cla=small.place_id
-data=small.drop('place_id',1)
-data=data.drop('accuracy',1)
-data=data.drop('row_id',1)
+
+y_train=X_train.place_id
+X_train=X_train.drop('place_id',1)
+X_train_acc=X_train.accuracy
+X_train=X_train.drop('accuracy',1)
+X_train=X_train.drop('row_id',1)
 clf = neighbors.KNeighborsClassifier(5)
-NearestNeighbors
-ft=clf.fit(data, cla)
-traintest=train[14559011:]
-traintest=traintest.drop('accuracy',1)
-traintest=traintest.drop('row_id',1)
-traintiny=getSquare(traintest,1,1)
-seek=traintiny['place_id']
-traintiny=traintiny.drop('place_id',1)
-res=clf.predict(traintiny)
-bol=(res==seek)
+
+ft=clf.fit(X_train, y_train)
+X_test_acc=X_test.accuracy
+X_test=X_test.drop('accuracy',1)
+X_test=X_test.drop('row_id',1)
+y_test=X_test['place_id']
+X_test=X_test.drop('place_id',1)
+res=clf.predict(X_test)
+bol=(res==y_test)
 fin=(1./bol.size)*sum(bol)
+
 ##may want to link accuracy to below somehow
-nbrs=NearestNeighbors(5,algorithm='ball_tree').fit(data)
-distances,indices=nbrs.kneighbors(traintiny)
+nbrs=NearestNeighbors(5,algorithm='ball_tree').fit(X_train)
+distances,indices=nbrs.kneighbors(X_test)
+knearest_locs=np.array(X_train)[indices]
+knearest_labels=np.array(y_train)[indices]
+knearest_acc=np.array(X_train_acc)[indices]
+points=np.array(X_test)
+reppoints=np.tile(points,(5,1,1))
+reppoints=reppoints.transpose((1,0,2))
+
+ty=knearest_locs-reppoints
+tys=np.square(ty)
+tyssum=np.sum(tys,axis=2)
+tyssums=np.sqrt(tyssum)
+tyssumsac=tyssums/knearest_acc
+eh=np.apply_along_axis(np.bincount,1,np.arange(5),weights=tyssumsac)
+np.bincount(np.arange(5),tyssumsac[0])
